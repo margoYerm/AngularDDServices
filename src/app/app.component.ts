48011e186e500c4f7de5cc4a049ec321fb7521df
Margo Yermachenko <margoermachenko@gmail.com>
2024-03-12 22:34:20 +0100

    Added http request with params as Observable. Show data in template with async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +13,17 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$: Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
+    const params = new HttpParams() //build in method
+      .set('page', '1')
+      .set('pageSize', '10')      //dev tools -> XHR -> http://localhost:4200/api/courses?page=1&pageSize=10     
+        //val => console.log(val)
+        this.courses$ = this.http.get<Course[]>('/api/courses', {params})      
   }
-
-
-
 }
