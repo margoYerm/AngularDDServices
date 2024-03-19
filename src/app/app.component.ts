49e5de3e9efbf40573706a82ad611a2620cb5d46
Margo Yermachenko <margoermachenko@gmail.com>
2024-03-19 19:04:35 +0100

    OnPush change detection and Observables data streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, InjectionToken, OnInit, Optional, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, InjectionToken, OnInit, Optional, inject} from '@angular/core';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {Observable} from 'rxjs';
@@ -10,18 +10,25 @@ import { COURSES } from 'src/db-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],  
+  styleUrls: ['./app.component.css'], 
+  //changeDetection: ChangeDetectionStrategy.OnPush  
 })
 
 export class AppComponent implements OnInit {
 
-  courses = COURSES;
+  courses$: Observable<Course[]>;
+  //courses: Course[];
 
   constructor(
     private coursesService: CoursesService,
-    @Inject(CONFIG_TOKEN) private config: AppConfig) {}
+    //@Inject(CONFIG_TOKEN) private config: AppConfig
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
+    /*this.coursesService.loadCourses() 
+      .subscribe(courses => this.courses = courses)*/
+  }
 
   saveValue(course: Course) {
     this.coursesService.saveCourse(course)
@@ -31,10 +38,10 @@ export class AppComponent implements OnInit {
   }
   
   onEditCourse() {
-    const course = this.courses[0];
+    /*const course = this.courses$[0];
     const newCourse = {...course};
     newCourse.description = 'New value!';
-    this.courses[0] = newCourse;
-    //this.courses[0].description = 'New value!'; // it's not work
+    this.courses$[0] = newCourse;
+    //this.courses[0].description = 'New value!'; // it's not work*/
   }  
 }
